ee3879dc9a9998a7367ff43c3533fb3d74832eba
EliaMagris <elia.magris@libero.it>
2022-10-18 13:36:23 +0200

    Esercizio completato

---
+++ assets/main.js
@@ -0,0 +1,12 @@
+let name = prompt("Write your name")
+console.log(name);
+
+let lastName = prompt("Write your last name")
+console.log(lastName);
+
+let favouriteColor = prompt("Write your favourite color")
+console.log(favouriteColor);
+
+const number = 88;
+
+document.getElementById("password-result").innerHTML = `your password is ${name}${lastName}${favouriteColor}${number}`
